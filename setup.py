656844f5eca98f915d9206a2699fc77b1925ddae
Zygmunt Krynicki <me@zygoon.pl>
2016-02-06 22:53:34 +0100

    Mark Python 3.3-3.5 as supported
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- setup.py
+++ setup.py
@@ -40,6 +40,9 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     install_requires=[
         'simplejson >= 2.0.9'],
