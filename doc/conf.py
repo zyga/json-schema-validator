f5f262ba6cf7280e52ee0d86bcbd7168fd931114
Zygmunt Krynicki <me@zygoon.pl>
2016-02-07 00:37:12 +0100

    Update documentation copyright
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'JSON Schema Validator'
-copyright = u'2010, 2011 Linaro Limited'
+copyright = u'2010, 2011 Linaro Limited; 2016 Zygmunt Krynicki'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
